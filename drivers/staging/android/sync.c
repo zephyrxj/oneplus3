28c913bc60678df74464c11f2592564066d83220
Ronaldi <48817684+zephyrxj@users.noreply.github.com>
2019-07-07 20:14:42 +0700

    Update sync.c

--- drivers/staging/android/sync.c
+++ drivers/staging/android/sync.c
@@ -638,7 +638,6 @@ EXPORT_SYMBOL(sync_fence_wait);
 static void sync_fence_free(struct kref *kref)
 {
 	struct sync_fence *fence = container_of(kref, struct sync_fence, kref);
-	int i;
 
 	sync_fence_free_pts(fence);
 
